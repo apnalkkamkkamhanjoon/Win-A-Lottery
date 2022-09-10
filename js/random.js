125ee75388e8c42964af01bc84fa251834370f64
cutejoon <s22073@gsm.hs.kr>
2022-09-10 20:00:37 +0900

    timer good

--- js/random.js
+++ js/random.js
@@ -1,9 +1,15 @@
 const page = document.querySelector("h1");
-let time = 6;
+let time = 3;
+let min = "";
 let sec = "";
 let x = setInterval(function(){
+    min = time/60;
     sec = time % 60;
 
     page.innerText = sec + "초 후 공개";
     time--;
-})
+    if(time < 0){
+        clearInterval(x);
+        page.innerText = "번호";
+    }
+},1000);
