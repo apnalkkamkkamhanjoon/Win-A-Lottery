4ea98c36cd007f79169978a7ac755198ce2c504b
cutejoon <s22073@gsm.hs.kr>
2022-09-11 22:14:12 +0900

    0911f

--- js/random.js
+++ js/random.js
@@ -1,15 +1,25 @@
 const page = document.querySelector("h1");
+let randomIndex = [];
 let time = 3;
 let min = "";
 let sec = "";
+for(i = 0; i <6; i++){
+    const randomNumber = Math.floor(Math.random() * 45);
+    if(randomIndex.indexOf(randomNumber) === -1){
+        randomIndex.push(randomNumber);
+    }
+    else{
+        i--;
+    }
+}
 let x = setInterval(function(){
     min = time/60;
     sec = time % 60;
 
-    page.innerText = sec + "초 후 공개";
+    page.innerText = sec + "";
     time--;
     if(time < 0){
         clearInterval(x);
-        page.innerText = "번호";
+        page.innerHTML = randomIndex;
     }
 },1000);
