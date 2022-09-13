3b31fb82080b8b0bed81da4a4dfcef60eb251156
cutejoon <s22073@gsm.hs.kr>
2022-09-13 20:28:42 +0900

    제목이..

--- js/random.js
+++ js/random.js
@@ -1,10 +1,10 @@
 let randomIndex = [];
-for(j = 0; j < 6; j++){
+for(i = 0; i < 6; i++){
     const randomNumber = Math.floor(Math.random() * 45 + 1);
     if(randomIndex.indexOf(randomNumber) === -1){
             randomIndex.push(randomNumber);
     }
     else{
-        j--;
+        i--;
     }
 }
--- js/timer.js
+++ js/timer.js
@@ -1,16 +1,19 @@
-const page = document.querySelector("h1");
+const page = document.querySelector(".randomnum");
 let time = 3;
 let min = "";
 let sec = "";
 
+function lay (f, s) { 
+    return f-s;
+};
+
 let x = setInterval(function(){
     min = time/60;
     sec = time % 60;
-
     page.innerText = "생성중...(" + sec + ")";
     time--;
     if(time < 0){
         clearInterval(x);
-        page.innerHTML = randomIndex.sort(function (f, s) { return f-s; });
+        page.innerHTML = randomIndex.sort(lay);
     }
 },1000);
